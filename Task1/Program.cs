a3ee16b84be895d3830d19269f9b8d9db3269be4
Your Name <you@example.com>
2022-10-30 11:08:34 +0300

    All good

---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+void FillMassiv(int[,] massiv)
+{
+    Random num = new Random();
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = num.Next(-10, 11);
+        }
+    }
+}
+
+void PrintMass(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MassSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] >= array[i, k])
+                {
+                    int temporary = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temporary;
+                }
+            }
+        }
+    }
+
+}
+
+Console.Write("Введите число (строк) m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число (столбцов) n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] array = new int[m, n];
+FillMassiv(array);
+PrintMass(array);
+Console.WriteLine();
+MassSort(array);
+PrintMass(array);
---
+++ Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+
+void FillMassiv(int[,] massiv)
+{
+    Random num = new Random();
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = num.Next(-10, 11);
+        }
+    }
+}
+
+void PrintMass(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeStroktoStolb(int[,] massiv)
+{
+    int[,] newmassiv = new int[massiv.GetLength(0), massiv.GetLength(1)];
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            newmassiv[i, j] = massiv[j, i];
+        }
+    }
+    return newmassiv;
+}
+        
+Console.Write("Введите число (строк) m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число (столбцов) n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] array = new int[m, n];
+int[,] changedArray = new int[m, n];
+FillMassiv(array);
+PrintMass(array);
+Console.WriteLine();
+if (m == n)
+{
+    changedArray = ChangeStroktoStolb(array);
+    PrintMass(changedArray);
+}
+else Console.WriteLine("Матрица не квадратная");
